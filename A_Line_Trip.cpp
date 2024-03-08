37a03ec2383089dc2da0953c80d316966e1c67cd
CV.LIKHITH <143379120+CodeJeet2005LIKHITH@users.noreply.github.com>
2024-03-08 22:23:14 +0530

    commit
    A Line Trip Question

---
+++ A_Line_Trip.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int tc;
+    cin>>tc;
+    while(tc--){
+        int n,x;
+        cin>>n>>x;
+        int arr[n];
+        for(int i=0; i < n; i++)cin>>arr[i];
+        // You need find the max distance between at any two points
+        // =>Distance between origin and the first coordinate
+        // =>Distance in between two gas stations
+        // =>Distance between the last gas station and the end point
+        int disfirst = arr[0]-0;
+        int dislast = x-arr[n-1];
+        int dismid = 0;
+        
+        for(int i=0; i < n-1; i++)dismid = max(dismid,arr[i+1]-arr[i]);
+        cout<<max(disfirst,max(2*dislast,dismid))<<endl;
+    }
+}
